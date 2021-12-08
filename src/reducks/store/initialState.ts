e53b17369bea0f37d73e1c500ab64860c824a010
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-08 15:18:43 +0900

    initialState(Storeの初期状態)を定義

---
+++ src/reducks/store/initialState.ts
@@ -0,0 +1,16 @@
+// initialState 
+// Storeの初期状態を定義する
+
+const initialState = {
+  // カテゴリに分けて、オブジェクトで記述
+  products: {
+
+  },
+  users: {
+    isSignedIn: false,
+    uid: '',
+    username: ''
+  }
+};
+
+export default initialState
