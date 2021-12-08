89f7e2dabaa967c42591175dc2456a2ce7e29b0f
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-08 15:18:09 +0900

    usersのReducerを作成

---
+++ src/reducks/users/reducers.ts
@@ -0,0 +1,25 @@
+import * as Actions from './actions';
+import initialState from '../store/initialState';
+
+type Action = {
+  type: string;
+  payload: any;
+}
+ 
+// 第一引数にState(現在のstateの状態)。初期状態をデフォルトとして設定
+// 第二引数にactionがreturnした値(typeとpayload)
+export const UsersReducer = (state = initialState.users, action: Action) => {
+  // Actionsのtypeに応じてstateをどう変更するのか決める
+  switch (action.type) {
+    case Actions.SIGN_IN:
+      // スプレット構文で展開してマージ
+      // stateの中身をaction.payloadで上書き
+      // action.payloadで指定されていない値がstoreから消えてしまうことを防ぐ
+      return {
+        ...state,
+        ...action.payload
+      }
+    default:
+      return state;
+  }
+}
