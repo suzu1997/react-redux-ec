4bad4e2da1a9abfdeb950c5441d7521751adaa77
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-13 13:53:04 +0900

    商品一覧ページをルートページの画面にルーティング設定

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router';
 
 import { SignIn, Home, SignUp, ResetPass, ProductEdit } from './templates';
 import { Auth } from './Auth';
+import ProductList from './templates/ProductList';
 
 export const Router = () => {
   return (
@@ -11,7 +12,8 @@ export const Router = () => {
       <Route exact path={'/signin/resetPassword'} component={ResetPass} />
       {/* 認証が必要なページをAuthコンポーネントで囲んで監視 */}
       <Auth>
-        <Route exact path={'(/)?'} component={Home} />
+        {/* ルートパスで商品一覧を表示 */}
+        <Route exact path={'(/)?'} component={ProductList} />
         {/* /product/editは商品の新規追加URL */}
         {/* /product/edit/:id は既存商品の編集URL*/}
         <Route path={'/product/edit(/:id)?'} component={ProductEdit} />
