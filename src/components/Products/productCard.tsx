9c81c6af036d4028f1c5d04fbe56f4e5ff508133
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-13 16:00:17 +0900

    商品カードの編集、メニューの追加

--- src/components/Products/productCard.tsx
+++ src/components/Products/productCard.tsx
@@ -1,8 +1,20 @@
-import { Card, CardContent, CardMedia, Typography } from '@material-ui/core';
-import { VFC } from 'react';
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+} from '@material-ui/core';
+import { useState, VFC } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { useDispatch } from 'react-redux';
 import { push } from 'connected-react-router';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+import NoImage from '../../assets/img/no_image.png';
+import { deleteProduct } from '../../reducks/products/operations';
 
 type Props = {
   product: any;
@@ -21,7 +33,7 @@ const useStyles = makeStyles((theme) => ({
   },
   content: {
     display: 'flex',
-    flexDirection: 'column',
+    justifyContent: 'space-between',
     padding: '16px 8px',
     textAlign: 'left',
     '&:lastchild': {
@@ -42,22 +54,60 @@ export const ProductCard: VFC<Props> = (props) => {
   const classes = useStyles();
   const dispatch = useDispatch();
 
+  // メニュー開閉する対象の要素
+  const [anchorEl, setAnchorEl] = useState<
+    Element | ((element: Element) => Element) | null | undefined
+  >(null);
+
+  // メニューを開く
+  const handleClick = (e: any) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  // メニューを閉じる
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const image =
+    props.product.images.length > 0 ? props.product.images[0].path : NoImage;
+
   const price = props.product.price.toLocaleString();
 
   return (
     <Card className={classes.root}>
       <CardMedia
         className={classes.media}
-        image={props.product.images[0].path}
+        image={image}
         onClick={() => dispatch(push('/product/' + props.product.id))}
       />
       <CardContent className={classes.content}>
-        <Typography color='textSecondary' component='p'>
-          {props.product.name}
-        </Typography>
-        <Typography className={classes.price} component='p'>
-          ¥{price}
-        </Typography>
+        <div>
+          <Typography color='textSecondary' component='p'>
+            {props.product.name}
+          </Typography>
+          <Typography className={classes.price} component='p'>
+            ¥{price}
+          </Typography>
+        </div>
+        <IconButton onClick={handleClick}>
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem
+            onClick={() => dispatch(push('/product/edit/' + props.product.id))}
+          >
+            編集する
+          </MenuItem>
+          <MenuItem onClick={() => dispatch(deleteProduct(props.product.id))}>
+            削除する
+          </MenuItem>
+        </Menu>
       </CardContent>
     </Card>
   );
