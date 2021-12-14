db0805bec857338f62ca1ffa5996af020b7794ca
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-14 13:25:50 +0900

    templateのエントリポイントファイルを更新

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,5 +1,8 @@
+export {default as CartList} from './CartList';
 export {default as Home} from './Home';
-export {default as SignUp} from './SignUp';
-export {default as SignIn} from './SignIn';
-export {default as ResetPass} from './ResetPass';
+export {default as ProductDetail} from './ProductDetail';
 export {default as ProductEdit} from './ProductEdit';
+export {default as ProductList} from './ProductList';
+export {default as ResetPass} from './ResetPass';
+export {default as SignIn} from './SignIn';
+export {default as SignUp} from './SignUp';
