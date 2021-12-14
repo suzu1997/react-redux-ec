03aff13fd1815470de520d6ad92bcb5beaa7ea96
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-14 12:02:44 +0900

    firebaseのカート内データのリスナーを設定

--- src/components/Header/HeaderMenus.tsx
+++ src/components/Header/HeaderMenus.tsx
@@ -1,19 +1,69 @@
 import { Badge } from '@material-ui/core';
 import { IconButton } from '@mui/material';
-import { VFC } from 'react';
+import { useEffect, VFC } from 'react';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import MenuIcon from '@material-ui/icons/Menu';
+
+import { getProductsInCart, getUserId } from '../../reducks/users/selectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../reducks/store/store';
+import { db } from '../../firebase';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { ProductInCart } from '../../reducks/users/types';
+import { fetchProductsInCart } from '../../reducks/users/operations';
 
 type Props = {
   handleDrawerToggle: (e: any) => void;
 };
 
 export const HeaderMenus: VFC<Props> = (props) => {
+  const dispatch = useDispatch();
+  const selector = useSelector((state: RootState) => state);
+  const uid = getUserId(selector);
+  let productsInCart = getProductsInCart(selector);
+
+  // firebaseのリスナーを設定
+  useEffect(() => {
+    const ref = collection(db, 'users', uid, 'cart');
+    // onSnapshotでリスナーを設定
+    const unsubscribe = onSnapshot(ref, (snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        // change.doc.data()にデータが入る
+        const product = change.doc.data();
+        // change.typeでどんな変化があったのか判別
+        const changeType = change.type;
+
+        console.log({ change });
+
+        // change.typeに応じて配列操作
+        if (changeType === 'added') {
+          productsInCart.push(product);
+        }
+        if (changeType === 'modified') {
+          const index = productsInCart.findIndex(
+            (product: ProductInCart) => product.cartId === change.doc.id
+          );
+          productsInCart[index] = product;
+        }
+        if (changeType === 'removed') {
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+          productsInCart = productsInCart.filter(
+            (product: ProductInCart) => product.cartId !== change.doc.id
+          );
+        }
+      });
+
+      dispatch(fetchProductsInCart(productsInCart));
+    });
+    // リスナーの解除
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <IconButton>
-        <Badge badgeContent={3} color='secondary'>
+        <Badge badgeContent={productsInCart.length} color='secondary'>
           <ShoppingCartIcon />
         </Badge>
       </IconButton>
