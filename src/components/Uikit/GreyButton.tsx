e166d3c521212d62001a0b54ed518530843012c8
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-14 15:41:17 +0900

    ボタンコンポーネントの作成と更新

--- src/components/Uikit/GreyButton.tsx
+++ src/components/Uikit/GreyButton.tsx
@@ -2,28 +2,23 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core';
 import type { MouseEventHandler, VFC } from 'react';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   button: {
-    backgroundColor: 'skyblue',
-    opacity: 0.9,
+    backgroundColor: '#ddd',
     color: '#000',
     fontSize: 16,
     height: 48,
     marginBottom: 16,
     width: 256,
-    '&:hover': {
-      backgroundColor: 'skyblue',
-      opacity: 1.0,
-    },
   },
-});
+}));
 
 type Props = {
   label: string;
   onClick: MouseEventHandler<HTMLButtonElement>;
 };
 
-export const PrimaryButton: VFC<Props> = (props: any) => {
+export const GreyButton: VFC<Props> = (props: any) => {
   const classes = useStyles();
 
   return (
