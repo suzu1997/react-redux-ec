18681538926267f3c735e10434901885b60f0cf2
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-15 22:29:42 +0900

    Homeコンポーネントが不要だったため削除

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,5 +1,4 @@
 export { default as CartList } from './CartList';
-export { default as Home } from './Home';
 export { default as OrderConfirm } from './OrderConfirm';
 export { default as OrderHistory } from './OrderHistory';
 export { default as ProductDetail } from './ProductDetail';
