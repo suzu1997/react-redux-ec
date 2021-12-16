58417d3cb65d4d5816a4674a4a29717f5dc22d88
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-16 09:54:11 +0900

    フッターコンポーネントを作成

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className='h-10 w-full bg-gray-300 text-center'>
+      <small className='leading-10'>&copy;suzu 2021</small>
+    </footer>
+  )
+}
