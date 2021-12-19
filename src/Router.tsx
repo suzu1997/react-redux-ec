47e4c392cec9e55959e4d5ccf029bc969930ce35
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-19 19:41:07 +0900

    ログインしていなくても商品を閲覧できるようにした

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,7 +8,9 @@ import {
   ProductList,
   ProductDetail,
   CartList,
+  FavoriteList,
   OrderConfirm,
+  OrderComplete,
   OrderHistory,
 } from './templates';
 import { Auth } from './Auth';
@@ -19,18 +21,19 @@ export const Router = () => {
       <Route exact path={'/signup'} component={SignUp} />
       <Route exact path={'/signin'} component={SignIn} />
       <Route exact path={'/signin/resetPassword'} component={ResetPass} />
+      <Route exact path={'(/)?'} component={ProductList} />
+      <Route exact path='/product/detail/:id' component={ProductDetail} />
       {/* 認証が必要なページをAuthコンポーネントで囲んで監視 */}
       <Auth>
         {/* ルートパスで商品一覧を表示 */}
-        <Route exact path={'(/)?'} component={ProductList} />
-        <Route exact path="/product/detail/:id" component={ProductDetail} />
         {/* /product/editは商品の新規追加URL */}
         {/* /product/edit/:id は既存商品の編集URL*/}
-        <Route path="/product/edit(/:id)?" component={ProductEdit} />
+        <Route path='/product/edit(/:id)?' component={ProductEdit} />
         <Route exact path={'/cart'} component={CartList} />
         <Route exact path={'/order/confirm'} component={OrderConfirm} />
         <Route exact path={'/order/complete'} component={OrderComplete} />
         <Route exact path={'/order/history'} component={OrderHistory} />
+        <Route exact path={'/favorite'} component={FavoriteList} />
       </Auth>
     </Switch>
   );
