3b7fe539de44ba6f1bfb5b805cad861d0f2d15a2
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-19 12:40:24 +0900

    フッターコンポーネントにコンポーネントの型定義を付与

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,6 @@
-export const Footer = () => {
+import { VFC } from 'react'
+
+export const Footer: VFC = () => {
   return (
     <footer className='h-10 w-full bg-gray-300 text-center'>
       <small className='leading-10'>&copy;suzu 2021</small>
