9e48fa8d56dd0f68116a5f92d3c6309ca829457c
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-21 00:03:56 +0900

    メインコンテンツの横幅を確保

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,13 @@ const App = () => {
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
-      <main className='py-24 flex flex-1'>
+      <main className='py-24 flex flex-1 min-w-screen'>
         <div className='hidden lg:block lg:w-60 ml-2'>
           <MenuColumn />
         </div>
-        <Router />
+        <div className='flex-1'>
+          <Router />
+        </div>
         <div className='hidden lg:block lg:w-60 -mt-60'>
           <img src={banner} alt='バナー' className='w-20 mx-auto' />
         </div>
