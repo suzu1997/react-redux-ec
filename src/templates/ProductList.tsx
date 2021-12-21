a4aa6e6ba223bfc6690c1b1dd5378c048b3b31f2
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-21 16:41:13 +0900

    ローディング処理を削除

--- src/templates/ProductList.tsx
+++ src/templates/ProductList.tsx
@@ -16,7 +16,6 @@ const ProductList: VFC = () => {
   const selector = useSelector((state: RootState) => state);
   const products = getProductList(selector);
 
-  const [isLoading, setIsLoading] = useState<boolean>(true);
   // 商品の並び順
   const [order, setOrder] = useState<string>('time');
 
@@ -72,37 +71,8 @@ const ProductList: VFC = () => {
       }
     });
     dispatch(fetchProducts(gender, category, keyword, order));
-
-    setIsLoading(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch, gender, category, keyword, order]);
-
-  if (isLoading) {
-    return (
-      <div>
-        <svg
-          className='animate-spin -ml-1 mr-3 h-5 w-5 text-blue-500'
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-        >
-          <circle
-            className='opacity-25'
-            cx='12'
-            cy='12'
-            r='10'
-            stroke='currentColor'
-            strokeWidth='4'
-          ></circle>
-          <path
-            className='opacity-75'
-            fill='currentColor'
-            d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-          ></path>
-        </svg>
-      </div>
-    );
-  }
 
   return (
     <section className='mx-auto my-0 max-w-xl py-0 px-4 text-center w-full sm:max-w-5xl'>
