f44b6b19031982c27aacc08e6804e9cda54e7900
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-21 09:48:04 +0900

    ユーザー情報を取得するselector関数を作成

--- src/reducks/users/selectors.ts
+++ src/reducks/users/selectors.ts
@@ -22,6 +22,11 @@ export const getProductsInCart = createSelector(
   (state) => state.cart
 )
 
+export const getUserInfo = createSelector(
+  [usersSelector],
+  (state) => state
+)
+
 export const getUserId = createSelector(
   [usersSelector],
   (state) => state.uid
